37ba8f70aa6076dc5099fa09f8c936fdb684f1c5
Diego Martins de Pinho <diegomartinsdepinho@msn.com>
2023-07-10 13:45:38 -0300

    week 3 - tests

--- src/protocols.ts
+++ src/protocols.ts
@@ -1,3 +1,5 @@
+import { Payment, Ticket } from '@prisma/client';
+
 export type ApplicationError = {
   name: string;
   message: string;
@@ -43,4 +45,20 @@ export type RequestError = {
   statusText: string;
   name: string;
   message: string;
+};
+
+export type CreateTicketParams = Omit<Ticket, 'id' | 'createdAt' | 'updatedAt'>;
+
+export type CardPaymentParams = {
+  issuer: string;
+  number: number;
+  name: string;
+  expirationDate: Date;
+  cvv: number;
+};
+
+export type PaymentParams = Omit<Payment, 'id' | 'createdAt' | 'updatedAt'>;
+
+export type InputTicketBody = {
+  ticketTypeId: number;
 };
