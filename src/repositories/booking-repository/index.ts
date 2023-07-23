0dc98ed8bedf573862759bcb7eff6dd5c4b79606
ThaisFReis <reis.thaisf@gmail.com>
2023-07-23 12:20:08 -0300

    Repositories alterados

---
+++ src/repositories/booking-repository/index.ts
@@ -0,0 +1,55 @@
+import { prisma } from '@/config';
+
+// Create a new booking
+async function createBooking(roomId: number, userId: number) {
+  return prisma.booking.create({
+    data: {
+      userId,
+      roomId,
+    },
+  });
+}
+
+// Find all bookings
+async function findBookings() {
+    return prisma.booking.findMany();
+}
+
+// Find a booking by user id
+async function findBookingByUserId(userId: number) {
+    return prisma.booking.findFirst({
+        where: {
+        userId,
+        },
+    });
+}
+
+// Find a booking by room id
+async function findBookingByRoomId(roomId: number) {
+    return prisma.booking.findFirst({
+        where: {
+        roomId,
+        },
+    });
+}
+
+// Update a booking by id
+async function updateBookingById(id: number, roomId: number, userId: number) {
+    return prisma.booking.update({
+        where: {
+        id,
+        },
+        data: {
+        userId,
+        roomId,
+        },
+    });
+}
+
+const bookingRepository = {
+    createBooking,
+    findBookings,
+    findBookingByUserId,
+    findBookingByRoomId,
+    updateBookingById
+};
